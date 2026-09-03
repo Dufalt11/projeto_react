19b6eb54c93111fab9d3f9f9b4a9d3e7f83c220f
Abel-dev <dufalt11@gmail.com>
2026-09-02 20:21:45 -0400

    primeira aplicação

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+export default function App() {
+  return(
+    <main className='App'>
+        <header className='hero'>
+          <p className='eyebrow'>My Daily Habits </p>
+          <h1>Pequenos Hábitos</h1>
+          <p>Hoje começamos com um tela simples e funcional</p>
+        </header>
+
+        <section className='habit-list' aria-label='hábitos de hoje'>
+          <article className='habit-card'>
+            <h2>Beber Água</h2>
+            <p>Meta: 30 minutos</p>
+          </article>
+
+          <article className='habit-card'>
+            <h2>Caminhar</h2>
+            <p>Meta: 20  minutos </p>
+          </article>
+        </section>
+    </main>
+  )
+}
